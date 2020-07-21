10109e873a6e413cc1693cc1bc7cb105f4f6888f
245142637@qq.com <asd+-*/00>
2020-07-21 17:55:24 +0800

    改一下实体

--- src/main/kotlin/com/zeongit/share/database/account/entity/UserInfo.kt
+++ src/main/kotlin/com/zeongit/share/database/account/entity/UserInfo.kt
@@ -21,39 +21,49 @@ class UserInfo() : BaseEntity(), Serializable {
     //性别
     @Column(name = "user_id")
     var userId: Int = 0
+
     //性别
     @Column(name = "gender")
     var gender: Gender = Gender.MALE
+
     //生日
     @Column(name = "birthday")
     var birthday: Date = Date()
+
     //昵称
     @Column(name = "nickname")
-    var nickname: String = "9527"
+    lateinit var nickname: String
+
     //简介
     @Column(name = "introduction")
-    var introduction: String = "大家好啊"
+    lateinit var introduction: String
+
     //国家
     @Column(name = "country", length = 32)
-    var country: String = "中国"
+    var country: String? = null
+
     //省份
     @Column(name = "province", length = 32)
     var province: String? = null
+
     //城市
     @Column(name = "city", length = 32)
     var city: String? = null
+
     //头像
     @Column(name = "avatar_url")
     var avatarUrl: String? = null
+
     //背景图
     @Column(name = "background")
     var background: String? = null
+
     //用户状态（冗余字段）
     @Column(name = "state")
     var state: UserState = UserState.PASS
 
-    constructor(gender: Gender = Gender.UNKNOWN, birthday: Date = Date(), nickname: String = "9527", introduction: String = "大家好啊",
-                country: String = "中国", province: String? = null, city: String? = null, avatarUrl: String? = null, background: String? = null) : this() {
+    constructor(gender: Gender = Gender.UNKNOWN, birthday: Date = Date(), nickname: String = "镜花水月", introduction: String = "镜花水月",
+                country: String? = null, province: String? = null, city: String? = null, avatarUrl: String? = null, background: String? = null) : this() {
         this.gender = gender
         this.birthday = birthday
         this.nickname = nickname
