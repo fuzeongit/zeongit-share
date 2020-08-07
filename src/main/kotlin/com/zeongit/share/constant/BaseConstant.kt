49b34abad6a8984ff5359c9108dcbafe89e06e59
245142637@qq.com <asd+-*/00>
2020-08-07 09:26:09 +0800

    去secdra化

--- src/main/kotlin/com/zeongit/share/constant/BaseConstant.kt
+++ src/main/kotlin/com/zeongit/share/constant/BaseConstant.kt
@@ -5,7 +5,7 @@ import java.util.*
 object BaseConstant {
     val JWT_SECRET = Base64.getEncoder().encodeToString("zeongit".toByteArray(charset("utf-8")))
 
-    val not_UUID = "00000000000000000000000000000000"
+    val NOT_UUID = "00000000000000000000000000000000"
 
     val NOT_ID = 0
 }
